8409e246dec77b86a47ac42d8fe8c30f740c53b7
Shauaib2580 <www.shauaib2580@gmail.com>
2024-10-02 10:00:52 +0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import DaisyNav from './component/DaisyNav/DaisyNav';
 
 function App() {
   return (
     <>
+      <DaisyNav></DaisyNav>
       <h1 className="text-7xl bg-rose-400">Vite+React</h1>
-
+      
       
     </>
   );
